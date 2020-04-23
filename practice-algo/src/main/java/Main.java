74b793679825e49262a48d7c6d0942e34cfb034e
tinhtinhcd <tinhtinhcd@gmail.com>
2020-04-23 15:14:22 -0500

    RemoveDuplicatesSortedArray

--- practice-algo/src/main/java/Main.java
+++ practice-algo/src/main/java/Main.java
@@ -3,15 +3,15 @@ public class Main {
         removeDuplicatesFromSortedArray();
     }
 
-    //Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
-    //Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
-    //
-    //Example 1:
-    //Given nums = [1,1,2],
-    //Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
-    //It doesn't matter what you leave beyond the returned length.
-    private static void removeDuplicatesFromSortedArray(){
+    //    Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+//    Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+//
+//    Example 1:
+//    Given nums = [1,1,2],
+//    Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+//    It doesn't matter what you leave beyond the returned length.
+    private static void removeDuplicatesFromSortedArray() {
         RemoveDuplicatesSortedArray rm = new RemoveDuplicatesSortedArray();
-        rm.remove();
+        rm.remove(new int[]{1, 1, 2, 3, 3, 4, 5, 5, 5, 5, 6});
     }
 }
--- practice-algo/src/main/java/RemoveDuplicatesSortedArray.java
+++ practice-algo/src/main/java/RemoveDuplicatesSortedArray.java
@@ -1,6 +1,23 @@
+import java.util.Arrays;
+
 public class RemoveDuplicatesSortedArray {
 
-    public void remove(){
+    public void remove(int[] nums) {
+        int j = 0;
+        for (int i = 1; i < nums.length ; i++){
+            if(nums[i] != nums[j]){
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        int[] rs = new int[j+1];
+        System.arraycopy(nums, 0, rs, 0, j+1);
+        System.out.printf(Arrays.toString(rs));
+    }
 
+    // using java 8 + stream
+    public void removeV2(int[] nums){
+        nums = Arrays.stream(nums).distinct().toArray();
+        System.out.printf(Arrays.toString(nums));
     }
 }
